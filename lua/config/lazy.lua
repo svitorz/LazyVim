3143543747e0c0ad4267d2e252aacfc51af5691e
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-09-07 13:06:29 -0300

    Add my first nvim configs

---
+++ lua/config/lazy.lua
@@ -0,0 +1,72 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- php lsp
+    { import = "lazyvim.plugins.extras.lang.php" },
+    -- git
+    { import = "lazyvim.plugins.extras.lang.git" },
+    --java
+    { import = "lazyvim.plugins.extras.lang.java" },
+    --markdown
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    --sql
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    -- tailwind
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    --vuejs
+    { import = "lazyvim.plugins.extras.lang.vue" },
+    --eslint
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    --prettier
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- Lua
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
